60c00d39abe567286b6294465db58c8199982403
Irina Krotova <i.v.krotova@gmail.com>
2025-12-01 00:11:23 +0100

    refactor: move helpers function to a separated module

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod helpers {
 
     /// Public greeting function.
     pub fn greet() {
+        // pub helper function is accessible from the main module
         // returns unit type () by default, which is similar to void in other languages
         println!("Hello, world!");
     }
